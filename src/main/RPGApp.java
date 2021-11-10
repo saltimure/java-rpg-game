e4f739f7a78066b467e240a6d133e7a365c8f534
saltimure <saltimure@gmail.com>
2021-11-10 17:22:53 +0300

    gui 2

--- src/main/RPGApp.java
+++ src/main/RPGApp.java
@@ -12,52 +12,13 @@ import javafx.stage.Stage;
 public class RPGApp extends Application{
 
 	public static void main(String[] args) {
-		
 		Application.launch(args);
-		/*
-		Scanner scan = new Scanner(System.in);
-		Event event = new Event();
-		User user = new User();
-		while(user.getHealth() > 0) {
-			System.out.println("1.Go to the dungeon" + "\n" + "2.Check inventory"
-					+ "\n" + "3.Check level"  + "\n" + "4.Check progress"  + "\n" + "5.Check stats"
-					+ "\n" + "6.Restart game" + "\n" + "7.Exit game");
-			switch (scan.nextInt()) {
-			case 1:
-				System.out.println("Which difficulty do you want?");
-				System.out.println(event.fight(user, new Enemy(scan.nextInt())));
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				System.out.println(user.getLevel());
-				break;
-			case 4:
-				System.out.println(user.getProgress());
-				break;
-			case 5:
-				System.out.println("Your attack: " + user.getAttack());
-				System.out.println("Your health: " + user.getHealth());
-				break;
-			case 6:
-				user = new User();
-				break;
-			case 7:
-				user.setHealth(0);
-				break;
-			default:
-				break;
-			}
-		}
-		*/
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Scene1.fxml"));
 		Scene scene = new Scene(root);
-		
 		stage.setTitle("saltimure");
 		stage.setResizable(false);
 		stage.setScene(scene);
--- src/main/SceneController.java
+++ src/main/SceneController.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -18,12 +21,18 @@ public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	
 	private User user = new User();
 	private int difficulty = 1;
 	private String consoleInfo = "";
+	private ArrayList<Image> imgArList = new ArrayList<Image>();
 	
 	@FXML
 	private TextArea textArea;
+	@FXML
+	private ImageView imageView;
+	@FXML
+	private ImageView gallery;
 	
 	public void fightResult(ActionEvent e) {
 		if (user.fight(user, new Enemy(difficulty)) == "Player won") {
@@ -33,6 +42,11 @@ public class SceneController {
 			user = new User();
 		}
 		textArea.setText(consoleInfo);
+		if ((int)(Math.random() * (3 - 1) + 1) == 1) {
+			Image image = new Image((int)(Math.random() * (3 - 1) + 1) + ".jpeg");		
+			imgArList.add(image);
+			imageView.setImage(image);
+		}
 	}
 	
 	public void restart(ActionEvent e) {
